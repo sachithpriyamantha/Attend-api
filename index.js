1638939d1b0409708ef1f944e8a1ad5da5090c2d
Sachith Priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2024-10-03 20:48:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-const express = require("express")
-const cors = require("cors")
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+const app = express();
+const Routes = require("./routes/route.js");
 
-const app = express()
-const Routes = require("./routes/route.js")
-
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000; // Corrected port
 
 dotenv.config();
 
-
-app.use(express.json({ limit: '10mb' }))
+app.use(express.json({ limit: '10mb' }));
 app.use(cors({
     origin: ["https://attend-tuition.vercel.app"],
-    methods: ["GET", "POST","PUT","DELETE"],
-    credentials:true
-}))
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
 mongoose
     .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
-    .then(console.log("Connected to MongoDB"))
-    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err));
 
 app.use('/', Routes);
 
 app.listen(PORT, () => {
-    console.log(`Server started at port no. ${PORT}`)
-})
+    console.log(`Server started at port no. ${PORT}`);
+});
