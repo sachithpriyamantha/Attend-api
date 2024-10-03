e6d3a1a8fec8b2587bec55af26c9e6524b38749b
Sachith Priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2024-10-03 20:38:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dotenv = require("dotenv")
 const app = express()
 const Routes = require("./routes/route.js")
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || https://attend-api.vercel.app/
 
 dotenv.config();
 
