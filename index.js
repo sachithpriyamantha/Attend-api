339d6905cb369550000c7ceade4475f0768624a1
Sachith Priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2024-10-03 20:55:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 app.use(express.json({ limit: '10mb' }));
 app.use(cors({
-    origin: ["https://attend-tuition.vercel.app"],
+    origin: "https://attend-tuition.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
