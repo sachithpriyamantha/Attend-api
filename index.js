db74517d9bc84bf237b976e44d4638947020f145
Sachith Priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2024-10-04 21:05:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,34 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 
-const app = express();
-const Routes = require("./routes/route.js");
 
-const PORT = process.env.PORT || 5000;
+const app = express()
+const Routes = require("./routes/route.js")
+
+const PORT = process.env.PORT || 5000
 
 dotenv.config();
 
-// Enable JSON parsing
-app.use(express.json());
 
-const cors = require('cors');
+app.use(express.json({ limit: '10mb' }))
 app.use(cors({
-  origin: 'https://attend-tuition.vercel.app', // your frontend origin
-  methods: ['GET', 'POST', 'OPTIONS'], // allowed methods
-  credentials: true, // if you're sending cookies or auth tokens
-  
-}));
+    origin: ["https://attend-tuition.vercel.app"],
+    methods: ["GET", "POST","PUT","DELETE"],
+    credentials: true
+}))
 
-
-// Handle preflight requests (OPTIONS)
-app.options('*', cors());
-
-// MongoDB Connection
 mongoose
     .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err));
+    .then(console.log("Connected to MongoDB"))
+    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
 
-// Route setup
 app.use('/', Routes);
 
-// Start the server
 app.listen(PORT, () => {
-    console.log(`Server started at port no. ${PORT}`);
-});
+    console.log(`Server started at port no. ${PORT}`)
+})
